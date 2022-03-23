8dbf71146c8a6dbaea5cbe1f12b870ba2d5642eb
Mark <git@github.com:AjalaMark/Portfolio.git>
2022-03-23 22:54:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed(".auto-type", {
+    strings : ["Web Developer", "FreeLancer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true 
+})
+
+var typed2 = new Typed(".auto-type2", {
+    strings : ["Web Developer", "FreeLancer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true 
+})
