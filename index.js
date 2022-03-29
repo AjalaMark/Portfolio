aa9db275acee496f1faa41ff62762cc8f96b30d1
Mark <git@github.com:AjalaMark/Portfolio.git>
2022-03-29 02:39:05 +0100

    third commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+window.addEventListener("scroll", function(){
+    let navbar = document.querySelector("nav");
+    navbar.classList.toggle("sticky", window.scrollY > 0)
+})
+
+let menubtn = document.querySelector(".menu-btn")
+let listitems = document.querySelector(".list-items")
+menubtn.addEventListener("click", function(){
+    listitems.classList.toggle("active")
+    menubtn.classList.toggle("active")
+})
 
 var typed = new Typed(".auto-type", {
     strings : ["Web Developer", "FreeLancer"],
